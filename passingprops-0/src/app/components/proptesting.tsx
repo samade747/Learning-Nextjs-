0c650cd4f6f4c9e70548572fa2442fef0dfd15d7
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-01 14:44:19 +0500

    244

--- passingprops-0/src/app/components/proptesting.tsx
+++ passingprops-0/src/app/components/proptesting.tsx
@@ -1,9 +1,26 @@
 
 
-const Proptesting = (props:any) => {
+
+const Proptesting = (props:{
+    name:string,
+    age:string,
+    rollno:string,
+    cell:string,
+    slot:string,
+    time:string
+}) => {
 
     return (
         <div>
+
+            <h1>Name: {props.name}</h1>
+            <h1>Age: {props.age}</h1>
+            <h1>Rollno: {props.rollno}</h1>
+            <h1>Cell: {props.cell}</h1>
+            <h1>Slot: {props.slot}</h1>
+            <h1>Time: {props.time}</h1>
+            
+
 
         </div>
     )
