620a4188e91c60728a47c17add9c613cdad62c24
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-01 12:40:12 +0500

    1240am 1 10 2024

--- catchallsegments-4/src/app/docs/[[...slug]]/page.tsx
+++ catchallsegments-4/src/app/docs/[[...slug]]/page.tsx
@@ -3,12 +3,14 @@ export default function Docs({ params }: {
         slug: string[]
     };
 }) {    
-    if{params.slug.length  === 2}{
-     return <h1>
-        Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
+    if(params.slug?.length  === 2){
+     return(
+     <h1>
+       Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
                 
      </h1>
-    } else if (params.slug.length  === 1){
+    )
+    } else if (params.slug?.length  === 1){
         return <h1>
         Viewing docs for feature {params.slug[0]}       
      </h1>
