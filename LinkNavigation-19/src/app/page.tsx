d2d2c28f52029c9883a82fea01ca50aafd654afb
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-05 21:25:05 +0500

    ads

--- LinkNavigation-19/src/app/page.tsx
+++ LinkNavigation-19/src/app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
 <h1>Welcome to Next.js by samad </h1>
+
+    <Link href="https://www.linkedin.com/in/samaddeveloper" target="_blank">
+    linkedin
+    </Link>
+
+    <Link href="/products">products</Link>
+
+
+    <Link href="/about">about</Link>
+
+
+
+
+
+
 
 <h2>www.linkedin.com/in/samaddeveloper</h2>
 
--- LinkNavigation-19/src/app/products/page.tsx
+++ LinkNavigation-19/src/app/products/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import Link from "next/link";
 
 export default function ProductList() {
     return (
         <div>
+            <Link href="/">Home</Link>
             <h1>Product List</h1>
             <h1>Product 1</h1>
             <h1>Product 2</h1>
