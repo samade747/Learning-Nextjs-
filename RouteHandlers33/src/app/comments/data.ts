7968df42bcd84c5533a0ed4b05d7e4556a783ab5
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-12 13:07:38 +0500

    12:37am  get request

---
+++ RouteHandlers33/src/app/comments/data.ts
@@ -0,0 +1,18 @@
+export const comments = [
+    {
+        id: 1,
+        body: "comment 1",
+        postId: 1,
+    },
+    {
+        id: 2,
+        body: "comment 2",
+        postId: 1,
+    },
+    {
+        id: 3,
+        body: "comment 3",
+        postId: 2,
+    }
+
+]
