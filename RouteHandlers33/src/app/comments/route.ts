93b5d230fa88b7c667ae2396a4917d7021d8d14a
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-13 00:55:17 +0500

    1255

--- RouteHandlers33/src/app/comments/route.ts
+++ RouteHandlers33/src/app/comments/route.ts
@@ -1,8 +1,28 @@
+import { headers } from "next/headers";
 import { comments } from "./data";
 
 
 export async function GET() {
 
     return Response.json(comments)
+
+}
+
+
+
+export async function POST(request: Request) {      
+    const comment = await request.json()
+    const newcomment = {
+        id: comments.length + 1,
+        text: comment.text,
+        
+    } 
+comments.push(newcomment);
+    return new Response(JSON.stringify(newcomment), {
+    headers: {
+        "content-type": "application/json",
+    },    
+    status: 201,
+});
 
 }
