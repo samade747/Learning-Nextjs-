e4670ad0d9c4adfdc19664cf142cf8ffa969b098
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-16 18:46:35 +0500

    5

---
+++ DynamicRouteHandlers36/src/app/comments/data.ts
@@ -0,0 +1,18 @@
+export const comments = [
+    {
+        id: 1,
+        body: "comment SAMAD 1",
+        postId: 1,
+    },
+    {
+        id: 2,
+        body: "comment SAMAD 2",
+        postId: 1,
+    },
+    {
+        id: 3,
+        body: "comment SAMAD 3",
+        postId: 2,
+    }
+
+]
