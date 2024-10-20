8762bc4bad4cab906d8d878585e1acdae9075b6a
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-21 01:56:12 +0500

    136

---
+++ HandlingDELETERequest38/src/app/comments/data.ts
@@ -0,0 +1,18 @@
+export const comments = [
+    {
+        id: 1,
+        text: "sdd",
+        postId: 1,
+    },
+    {
+        id: 2,
+        text: "dsfdsdsds",
+        postId: 1,
+    },
+    {
+        id: 3,
+        text: "cdfdfdfs",
+        postId: 55,
+    }
+
+]
