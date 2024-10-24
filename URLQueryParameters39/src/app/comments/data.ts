6c70af4870b98488a23a71398f763031c9c5bcb9
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-25 00:51:18 +0500

    sadas

---
+++ URLQueryParameters39/src/app/comments/data.ts
@@ -0,0 +1,18 @@
+export const comments = [
+    {
+        id: 1,
+        text: "this is first comment",
+        
+    },
+    {
+        id: 2,
+        text: "dsfdsdsds ",
+        
+    },
+    {
+        id: 3,
+        text: "samad 33d ",
+        
+    }
+
+]
