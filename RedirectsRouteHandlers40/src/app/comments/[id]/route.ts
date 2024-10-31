ca0d5a853d58f7a09a19b72b3df802a1e488560e
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-31 14:19:39 +0500

    219

--- RedirectsRouteHandlers40/src/app/comments/[id]/route.ts
+++ RedirectsRouteHandlers40/src/app/comments/[id]/route.ts
@@ -5,7 +5,9 @@ import { comments } from "../data";
 export async function GET(_request: Request,
     { params }: { params: { id: string } }
 ) {
-
+    if(parseInt(params.id) > comments.length){
+        redirect("/comments");
+    } 
     const comment = comments.find(
         (comment) => comment.id === Number(params.id)
     );
